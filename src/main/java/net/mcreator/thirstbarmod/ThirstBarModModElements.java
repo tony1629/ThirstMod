4d64cf3898f7be1a197697e09a0c8848d2cf1199
tony1629 <anthonyl-19@hotmail.com>
2021-10-08 10:10:55 -0700

    -resized textures -started creation of empty bar

--- src/main/java/net/mcreator/thirstbarmod/ThirstBarModModElements.java
+++ src/main/java/net/mcreator/thirstbarmod/ThirstBarModModElements.java
@@ -13,6 +13,7 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.api.distmarker.Dist;
 
@@ -60,6 +61,7 @@ public class ThirstBarModModElements {
 		}
 		Collections.sort(elements);
 		elements.forEach(ThirstBarModModElements.ModElement::initElements);
+		MinecraftForge.EVENT_BUS.register(new ThirstBarModModVariables(this));
 	}
 
 	public void registerSounds(RegistryEvent.Register<net.minecraft.util.SoundEvent> event) {
