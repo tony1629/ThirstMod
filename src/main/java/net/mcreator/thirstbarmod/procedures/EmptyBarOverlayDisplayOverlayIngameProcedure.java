52d536eaca0a6f9ae72369edd1d5ab4446350391
tony1629 <74732215+Cursedwardog@users.noreply.github.com>
2021-11-09 09:13:15 -0800

    -does not compile

--- src/main/java/net/mcreator/thirstbarmod/procedures/EmptyBarOverlayDisplayOverlayIngameProcedure.java
+++ src/main/java/net/mcreator/thirstbarmod/procedures/EmptyBarOverlayDisplayOverlayIngameProcedure.java
@@ -11,33 +11,8 @@ public class EmptyBarOverlayDisplayOverlayIngameProcedure {
 				ThirstBarModMod.LOGGER.warn("Failed to load dependency entity for procedure EmptyBarOverlayDisplayOverlayIngame!");
 			return false;
 		}
-		if (dependencies.get("x") == null) {
-			if (!dependencies.containsKey("x"))
-				ThirstBarModMod.LOGGER.warn("Failed to load dependency x for procedure EmptyBarOverlayDisplayOverlayIngame!");
-			return false;
-		}
-		if (dependencies.get("y") == null) {
-			if (!dependencies.containsKey("y"))
-				ThirstBarModMod.LOGGER.warn("Failed to load dependency y for procedure EmptyBarOverlayDisplayOverlayIngame!");
-			return false;
-		}
-		if (dependencies.get("z") == null) {
-			if (!dependencies.containsKey("z"))
-				ThirstBarModMod.LOGGER.warn("Failed to load dependency z for procedure EmptyBarOverlayDisplayOverlayIngame!");
-			return false;
-		}
-		if (dependencies.get("world") == null) {
-			if (!dependencies.containsKey("world"))
-				ThirstBarModMod.LOGGER.warn("Failed to load dependency world for procedure EmptyBarOverlayDisplayOverlayIngame!");
-			return false;
-		}
 		Entity entity = (Entity) dependencies.get("entity");
-		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
-		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
-		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
-		IWorld world = (IWorld) dependencies.get("world");
-		if ((((world.getBlockState(new BlockPos((int) (x - 1), (int) (y - 1), (int) (z - 1)))).getBlock() == Blocks.WATER)
-				|| (((entity instanceof PlayerEntity) ? ((PlayerEntity) entity).getFoodStats().getFoodLevel() : 0) > 2))) {
+		if ((entity.isInWater())) {
 			return (false);
 		}
 		return (true);
