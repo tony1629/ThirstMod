7d6412a155d62014075b9297abe403cf4cd88c6b
tony1629 <74732215+Cursedwardog@users.noreply.github.com>
2021-12-16 09:44:17 -0800

    -created first timer -

--- src/main/java/net/mcreator/thirstbarmod/procedures/Icon1ProcedureProcedure.java
+++ src/main/java/net/mcreator/thirstbarmod/procedures/Icon1ProcedureProcedure.java
@@ -1,13 +1,39 @@
 package net.mcreator.thirstbarmod.procedures;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.TickEvent;
+
+import net.minecraft.world.World;
 import net.minecraft.entity.Entity;
 
-import net.mcreator.thirstbarmod.ThirstBarModModVariables;
 import net.mcreator.thirstbarmod.ThirstBarModMod;
 
 import java.util.Map;
+import java.util.HashMap;
 
 public class Icon1ProcedureProcedure {
+	@Mod.EventBusSubscriber
+	private static class GlobalTrigger {
+		@SubscribeEvent
+		public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+			if (event.phase == TickEvent.Phase.END) {
+				Entity entity = event.player;
+				World world = entity.world;
+				double i = entity.getPosX();
+				double j = entity.getPosY();
+				double k = entity.getPosZ();
+				Map<String, Object> dependencies = new HashMap<>();
+				dependencies.put("x", i);
+				dependencies.put("y", j);
+				dependencies.put("z", k);
+				dependencies.put("world", world);
+				dependencies.put("entity", entity);
+				dependencies.put("event", event);
+				executeProcedure(dependencies);
+			}
+		}
+	}
 	public static boolean executeProcedure(Map<String, Object> dependencies) {
 		if (dependencies.get("entity") == null) {
 			if (!dependencies.containsKey("entity"))
@@ -15,9 +41,13 @@ public class Icon1ProcedureProcedure {
 			return false;
 		}
 		Entity entity = (Entity) dependencies.get("entity");
-		if (((entity.isInWater()) && (((entity.getCapability(ThirstBarModModVariables.PLAYER_VARIABLES_CAPABILITY, null)
-				.orElse(new ThirstBarModModVariables.PlayerVariables())).water_amount) > 0))) {
+		if (((entity.getPersistentData().getDouble("ThirstBarTimer")) == 0)) {
 			return (false);
+		}
+		if (((entity.getPersistentData().getDouble("ThirstBarTimer")) == 0)) {
+			entity.getPersistentData().putDouble("ThirstBarTimer", 40);
+		} else {
+			entity.getPersistentData().putDouble("ThirstBarTimer", ((entity.getPersistentData().getDouble("ThirstBarTimer")) - 1));
 		}
 		return (true);
 	}
